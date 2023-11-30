6d73ca2d8053e8d02e36ab03fa0fd64c17443018
Shibenkova-Polina <134881551+Shibenkova-Polina@users.noreply.github.com>
2023-11-30 21:21:13 +0300

    change triangle

--- src/main/java/ru/mai/oop/figures/Triangle.java
+++ src/main/java/ru/mai/oop/figures/Triangle.java
@@ -19,24 +19,47 @@ public class Triangle extends Figure {
 
     @Override
     public boolean rightFigure() {
-        double firstLine = sqrt(pow((points[0].getX() - points[1].getX()), 2) + pow((points[0].getY() - points[1].getY()), 2)
-                + pow((points[0].getZ() - points[1].getZ()), 2));
-        double secondLine = sqrt(pow((points[2].getX() - points[1].getX()), 2) + pow((points[2].getY() - points[1].getY()), 2)
-                + pow((points[2].getZ() - points[1].getZ()), 2));
-        double thirdLine = sqrt(pow((points[0].getX() - points[2].getX()), 2) + pow((points[0].getY() - points[2].getY()), 2)
-                + pow((points[0].getZ() - points[2].getZ()), 2));
+        boolean check = true;
 
-        if (points[0].getX() == points[1].getX() && points[0].getY() == points[1].getY() && points[0].getZ() == points[1].getZ()
-                || points[0].getX() == points[2].getX() && points[0].getY() == points[2].getY() && points[0].getZ() == points[2].getZ()
-                || points[2].getX() == points[1].getX() && points[2].getY() == points[1].getY() && points[2].getZ() == points[1].getZ()) {
-            System.out.println("The figure is invalid");
-        } else if (firstLine + secondLine > thirdLine && firstLine + thirdLine > secondLine && secondLine + thirdLine > firstLine) {
-            System.out.println("The figure is valid");
-            return true;
+        int x0 = points[0].getX();
+        int y0 = points[0].getY();
+        int z0 = points[0].getZ();
+
+        cycle:
+        for (int j = 0; j < points.length - 2; j++) {
+            int x = points[j].getX();
+            int y = points[j].getY();
+            int z = points[j].getZ();
+
+            for (int i = j; i < points.length - 2; i++) {
+                int fCalculation = (points[i + 1].getY() - y) * (points[i + 2].getZ() - z) - (points[i + 2].getY() - y) * (points[i + 1].getZ() - z);
+                int sCalculation = -((points[i + 1].getX() - x) * (points[i + 2].getZ() - z)
+                        - (points[i + 2].getX() - x) * (points[i + 1].getZ() - z));
+                int tCalculation = (points[i + 1].getX() - x) * (points[i + 2].getY() - y) - (points[i + 2].getX() - x) * (points[i + 1].getY() - y);
+                if (fCalculation + sCalculation + tCalculation == 0) {
+                    check = false;
+                    break cycle;
+                }
+            }
+        }
+
+        if (check) {
+            double firstLine = sqrt(pow((points[0].getX() - points[1].getX()), 2) + pow((points[0].getY() - points[1].getY()), 2)
+                    + pow((points[0].getZ() - points[1].getZ()), 2));
+            double secondLine = sqrt(pow((points[2].getX() - points[1].getX()), 2) + pow((points[2].getY() - points[1].getY()), 2)
+                    + pow((points[2].getZ() - points[1].getZ()), 2));
+            double thirdLine = sqrt(pow((points[0].getX() - points[2].getX()), 2) + pow((points[0].getY() - points[2].getY()), 2)
+                    + pow((points[0].getZ() - points[2].getZ()), 2));
+
+            if (firstLine + secondLine > thirdLine && firstLine + thirdLine > secondLine && secondLine + thirdLine > firstLine) {
+                System.out.println("The figure is valid");
+                return true;
+            } else {
+                System.out.println("The figure is invalid");
+            }
         } else {
             System.out.println("The figure is invalid");
         }
-
         return false;
     }
 
